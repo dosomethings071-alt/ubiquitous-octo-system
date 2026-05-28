3b8a73838aacdef97bd993fa0ac41f1254b0a729
Sonal raj <Sonalormonal@gmail.com>
2026-05-28 22:53:55 +0530

    new featb

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function newFeature() {
     console.log('This is a new feature')
     console.log('this is a new game')
 }
+
+function newFeature() {
+    console.log('This is a new featureb')
+    console.log('this is a new gameb')
+}
